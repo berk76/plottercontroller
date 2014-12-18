5838b821d0fecb66697a428b4159e87d8327240b
berk <berk@beranek.webstones.cz>
2014-12-18 10:33:27 +0100

    PI def

--- src/graph.h
+++ src/graph.h
@@ -12,6 +12,13 @@ typedef struct {
 	double y;
 } D_POSITION;
 
+#ifndef M_PI
+#define M_PI		3.14159265358979323846	/* pi */
+#endif
+
+#ifndef M_PI_2
+#define M_PI_2		1.57079632679489661923	/* pi/2 */
+#endif
 
 /* Move Absolute */
 extern void xy_ma(PRINTER *p, int x, int y);
